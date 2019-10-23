4632627ba8922c6950557186c95b97ac46c58f1c
shreysaxena <shrey.30997@gmail.com>
2019-10-23 23:06:16 +0530

    First commit

---
+++ app/src/main/java/com/ssd/action/materialbutton/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ssd.action.materialbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        materialbutton materialbutton = findViewById(R.id.id1);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/ssd/action/materialbutton/materialbutton.java
@@ -0,0 +1,45 @@
+package com.ssd.action.materialbutton;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class materialbutton extends LinearLayout {
+
+    public materialbutton(Context context,  AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.material_button,this);
+        TypedArray array = context.obtainStyledAttributes(attrs,R.styleable.materialbutton,0,0);
+        Button button = findViewById(R.id.button);
+        init(array,button,context);
+
+    }
+
+    private void init(TypedArray array , Button b,Context c){
+        String text = array.getString(R.styleable.materialbutton_text);
+        boolean out = array.getBoolean(R.styleable.materialbutton_Outlined,false);
+
+
+
+        if (text == null){
+            b.setText("");
+        }else{
+            b.setText(text);
+        }
+        if (out){
+            b.setBackgroundResource(R.drawable.outlined_button_blue);
+            b.setTextColor(Color.parseColor("#6200ee"));
+        }else {
+            b.setBackgroundResource(R.drawable.button_blue);
+            b.setTextColor(Color.parseColor("#ffffff"));
+        }
+
+
+    }
+
+}
