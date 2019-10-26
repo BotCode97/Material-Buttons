45b4807395012550aec9e346fe8dd798c7d00952
shreysaxena <shrey.30997@gmail.com>
2019-10-26 13:45:56 +0530

    seventh commit

--- materialbutton2/src/main/java/com/ssd/action/materialbutton2/materialbutton.java
+++ materialbutton2/src/main/java/com/ssd/action/materialbutton2/materialbutton.java
@@ -33,7 +33,7 @@ public class materialbutton extends LinearLayout {
         }
         if (out){
             b.setBackgroundResource(R.drawable.outlined_button_blue);
-            b.setTextColor(Color.parseColor(String.valueOf(getResources().getColor(R.color.colorPrimary,null))));
+            b.setTextColor(Color.parseColor("#6200ee"));
         }else {
             b.setBackgroundResource(R.drawable.button_blue);
             b.setTextColor(Color.parseColor("#ffffff"));
